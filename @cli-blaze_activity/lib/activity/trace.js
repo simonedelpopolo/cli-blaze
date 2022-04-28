5e86a52429ca9da154eadc5a5cae2cde55ee71dd
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-04-28 11:00:42 +0000

    @cli-blaze/activity v1.0.2
    ADDED @cli-blaze/activity.trace function
      - wrapper for console.trace useful when in developing

---
+++ @cli-blaze_activity/lib/activity/trace.js
@@ -0,0 +1,9 @@
+/**
+ * Object [ activity.trace ]
+ * Trace something, prints values, Object or simply whatever.
+ *
+ * @param {...*} data - to be printed with console.trace
+ */
+export default function trace ( ...data ) {
+    console.trace( ...data )
+}
---
+++ tests/activity/trace.test.js
@@ -0,0 +1,19 @@
+import * as tttt from 'trythistrythat'
+import { trace } from '@cli-blaze/activity'
+
+/**
+ * Module filename - /Volumes/code/cli-blaze/tests/activity/trace.test.js
+ *
+ * @param {string} id - UNIT-test
+ * @returns {Promise<void> | void}
+ */
+export default async ( id ) => {
+
+    tttt.describe( '# trace something' )
+    await tttt.separator( 240, 75, '~' )
+    await tttt.line()
+
+    trace( 'hello', { object:'somewhat' }, new SyntaxError( 'trace this' ) )
+
+    tttt.end_test( id )
+}
